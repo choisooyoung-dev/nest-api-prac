309f028f805647b351d6a8270b5e4db8dd254765
sooyoung.choi <sooyoung.choi.dev@gmail.com>
2023-12-07 18:40:21 +0900

    🍻 test: git test

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -9,6 +9,7 @@ import {
 } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { Movie } from './entities/movie.entity';
+import { CreateMovieDto } from './dto/create-movie.dto';
 
 @Controller('movies')
 export class MoviesController {
@@ -24,23 +25,24 @@ export class MoviesController {
   //   return `We are searching for movie made after: ${searchingYear}`;
   // }
 
-  @Get('/:id')
-  getOne(@Param('id') movieId: string): Movie {
+  @Get(':id')
+  getOne(@Param('id') movieId: number): Movie {
+    console.log(typeof movieId);
     return this.moviesService.getOne(movieId);
   }
 
   @Post()
-  create(@Body() movieData) {
+  create(@Body() movieData: CreateMovieDto) {
     return this.moviesService.create(movieData);
   }
 
-  @Delete('/:id')
-  remove(@Param('id') movieId: string) {
+  @Delete(':id')
+  remove(@Param('id') movieId: number) {
     return this.moviesService.deleteOne(movieId);
   }
 
-  @Patch('/:id')
-  patch(@Param('id') movieId: string, @Body() updateData) {
-    return { updatedMovie: movieId, ...updateData };
+  @Patch(':id')
+  patch(@Param('id') movieId: number, @Body() updateData) {
+    return this.moviesService.update(movieId, updateData);
   }
 }
