85afdf0bc6d9b49bbfd87dc3a48e6b8baf562df2
sooyoung.choi <sooyoung.choi.dev@gmail.com>
2023-12-09 23:40:34 +0900

    ✨ update: Implements DTO(create, update)

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,10 +1,13 @@
-import { IsNumber, IsString } from 'class-validator';
+// DTO: Data Transfer Object
+
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class CreateMovieDto {
   @IsString()
   readonly title: string;
   @IsNumber()
   readonly year: number;
-  @IsString()
+  @IsString({ each: false })
+  @IsOptional()
   readonly genres: string[];
 }
